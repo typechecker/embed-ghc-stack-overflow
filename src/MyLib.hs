1ca90b655b618ae6fd3f5b350f964123aa0384b3
Phil de Joux <philderbeast@gmail.com>
2023-05-02 14:50:12 -0400

    Reproduction achieved.

--- src/MyLib.hs
+++ src/MyLib.hs
@@ -1,4 +1,14 @@
+{-# LANGUAGE TemplateHaskell #-}
+
 module MyLib (someFunc) where
 
+import Prelude hiding (putStrLn)
+import Data.ByteString (ByteString)
+import Data.ByteString.Char8 (putStrLn)
+import Data.FileEmbed (embedFile, makeRelativeToProject)
+
+file :: ByteString
+file = $(embedFile =<< makeRelativeToProject "data/big.csv")
+
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = putStrLn file
